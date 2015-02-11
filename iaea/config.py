2cb24fe43cd69e93add56478627900fbe65ffad7
Jaime García <jaime.garcia.llopis@gmail.com>
2015-02-11 16:19:15 +0100

    config: basic settings
    Signed-off-by: Jaime García <jaime.garcia.llopis@gmail.com>

---
+++ iaea/config.py
@@ -0,0 +1,20 @@
+import sys
+
+CFG_SITE_LANGS = ["en"]
+
+CFG_SITE_NAME = "INIS Input Management"
+CFG_SITE_NAME_INTL = {
+    "en": CFG_SITE_NAME
+}
+
+PACKAGES = [
+    "iaea.base",
+    "invenio.modules.*",
+]
+
+try:
+    from iaea import instance_config
+    sys.modules[__name__] = instance_config
+    del instance_config
+except ImportError:
+    pass
