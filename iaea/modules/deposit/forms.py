487dad3161377a40fb3d6eecb9053b2ea2cecf30
Jaime García <jaime.garcia.llopis@gmail.com>
2015-02-16 17:39:17 +0100

    deposit: file upload with extension validation
    * Added variable DEPOSIT_ACCEPTED_EXTENSIONS.
    
    * Validation based on file name, case-insensitive.
    
    Signed-off-by: Jaime García <jaime.garcia.llopis@gmail.com>

--- iaea/modules/deposit/forms.py
+++ iaea/modules/deposit/forms.py
@@ -1,12 +1,16 @@
 """Contains forms related to INIS submissions."""
 
+from flask import current_app, request
+
 from invenio.base.i18n import _
 
 from invenio.modules.deposit import fields
+from invenio.modules.deposit.field_widgets import plupload_widget
 from invenio.modules.deposit.filter_utils import strip_string
 from invenio.modules.deposit.form import WebDepositForm
 
 from wtforms import validators
+from wtforms.validators import ValidationError
 
 
 class UploadForm(WebDepositForm):
@@ -14,12 +18,14 @@ class UploadForm(WebDepositForm):
 
     title = fields.TitleField(
         label=_('Title'),
+        description='Optional.',
         widget_classes="form-control",
+        icon='fa fa-book fa-fw',
         export_key='title.title',
     )
 
     note = fields.TextAreaField(
-        label="Additional notes",
+        label=_("Notes"),
         description='Optional.',
         default='',
         validators=[validators.optional()],
@@ -30,6 +36,21 @@ class UploadForm(WebDepositForm):
         icon='fa fa-pencil fa-fw',
         export_key='notes',
     )
+
+    plupload_file = fields.FileUploadField(
+        label="",
+        widget=plupload_widget,
+        export_key=False
+    )
+
+    def validate_plupload_file(form, field):
+        """Ensure attached files have valid extensions."""
+        if not getattr(request, 'is_api_request', False):
+            # Tested in API by a separate workflow task.
+            for f in form.files:
+                if f.name[-4:].lower() not in current_app.config['DEPOSIT_ACCEPTED_EXTENSIONS']:
+                    raise ValidationError("All files must have one of the following extensions :" +
+                                          ', '.join(current_app.config['DEPOSIT_ACCEPTED_EXTENSIONS']))
 
     _title = _("Upload")
 
